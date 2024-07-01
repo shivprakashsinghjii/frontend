9e93f56fbb8f6ecfafc8f2c404d3d5fc65d7db56
Shiv Prakash Singh <144921828+shivprakashsinghjii@users.noreply.github.com>
2024-07-01 13:01:00 +0530

    start of task 1

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD08oTOCK4p5HI0RWmKcoWQ3NSnrAY6vg0",
+  authDomain: "login-auth-41900.firebaseapp.com",
+  projectId: "login-auth-41900",
+  storageBucket: "login-auth-41900.appspot.com",
+  messagingSenderId: "483740355480",
+  appId: "1:483740355480:web:c5ed43abd4cde52178b9ed",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
